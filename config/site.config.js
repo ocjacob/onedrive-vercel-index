0627cc86b302ffc47742454c1f8eb45cc5a11bd6
ocjacob <96604505+ocjacob@users.noreply.github.com>
2023-03-06 17:01:02 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -45,8 +45,7 @@ module.exports = {
   protectedRoutes: ['/Mshare', '/Private'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: 'mailto:mshare@microsoft365.hatu.work'
-',
+  email: 'mailto:mshare@microsoft365.hatu.work',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
